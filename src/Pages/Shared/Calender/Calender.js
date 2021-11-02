6e0f3465dd600ba69ac774d7d08e2c0d737d8379
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-11-02 23:41:34 +0600

    Calender add

--- src/Pages/Shared/Calender/Calender.js
+++ src/Pages/Shared/Calender/Calender.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
 
 const Calender = () => {
+
+    const [value, setValue] = React.useState(new Date());
+
     return (
-        <div>
-            Calender
-        </div>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <StaticDatePicker
+          orientation="landscape"
+          openTo="day"
+          value={value}
+          
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
     );
 };
 
